1a54584468426159ab97d76b560e59102a7d581e
DanBaDo <daniel.banobre.dopico@protonmail.com>
2021-11-08 01:18:44 +0000

    A kind of test

--- index.js
+++ index.js
@@ -13,4 +13,5 @@ const reloadMatrix = (rhs, internalResponse = []) => {
 
 // Tests
 const array = [[1,3,2],[12,1,3],[5,1,[1,2]],2];
-console.log(reloadMatrix(array));
+let output = JSON.stringify(reloadMatrix(array))
+console.log(output === "[1,3,2,12,1,3,5,1,1,2,2]");
