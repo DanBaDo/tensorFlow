22f9d12a2a9c825b53fdde96774610319bdd72d4
DanBaDo <daniel.banobre.dopico@protonmail.com>
2021-11-08 00:57:54 +0000

    Moving test array to tests section

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const array = [[1,3,2],[12,1,3],[5,1,[1,2]],2];
-
 function MatrixIndicators(value, position){
     this.value = value;
     this.position = position;
@@ -14,4 +12,5 @@ const reloadMatrix = (rhs, internalResponse = []) => {
 }
 
 // Tests
+const array = [[1,3,2],[12,1,3],[5,1,[1,2]],2];
 console.log(reloadMatrix(array));
