d2973dc4f45bbcc39feab60a03da9e3b5d3ec256
DanBaDo <daniel.banobre.dopico@protonmail.com>
2021-11-08 01:49:04 +0000

    Returning a list of matrixIndicators

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ function MatrixIndicators(value, position){
     this.position = position;
 }
 
-const reloadMatrix = (rhs, internalResponse = []) => {
-    if ( Array.isArray(rhs) ) rhs.forEach( rhs => reloadMatrix(rhs, internalResponse) )
+const reloadMatrix = (rhs,internalResponse = [], position=[]) => {
+    if ( Array.isArray(rhs) ) rhs.forEach(
+        (rhs, idx) => reloadMatrix(rhs, internalResponse, [...position, idx])
+    )
     else {
-        internalResponse.push(rhs);
+        internalResponse.push(new MatrixIndicators(rhs, position));
     }
     return internalResponse; // Kill current iteration keeping internalResponse
 }
 
+
 // Tests
-const array = [[1,3,2],[12,1,3],[5,1,[1,2]],2];
+const array = [[1,3,2],[12,1,3],[5,1,[1,2,[0,9,8,33]]],2];
 let output = JSON.stringify(reloadMatrix(array))
-console.log(output === "[1,3,2,12,1,3,5,1,1,2,2]");
+console.log(output)
